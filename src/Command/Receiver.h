354c52595f255eac59cf91825b771df0b76aeac2
Antoine Payan <antoine.payan@hotmail.fr>
2017-10-26 16:25:20 -0500

    Add basics of Command pattern

---
+++ src/Command/Receiver.h
@@ -0,0 +1,29 @@
+//
+// Created by Antoine Payan on 26/10/2017.
+//
+
+#ifndef IPLAYER_RECEIVER_H
+#define IPLAYER_RECEIVER_H
+
+#include <string>
+
+using namespace std;
+
+enum COMMAND_TYPE
+{
+    PLAY,
+    PAUSE,
+    CREATE_PLAYLIST,
+    NONE
+    // Add other commands
+};
+
+class Receiver {
+
+public:
+    virtual void setCommand(COMMAND_TYPE commandType, string parameter) = 0;
+    virtual string getResult() = 0;
+
+};
+
+#endif
