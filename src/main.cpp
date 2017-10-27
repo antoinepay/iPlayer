33fc63783471d3e5e7688932807989476e309d5f
Antoine Payan <antoine.payan@hotmail.fr>
2017-10-27 12:02:07 -0500

    Main implementation to receive commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
+#include "Model/Player.h"
+#include "Command/CommandParser.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Player *player = new Player();
+    string commandLine;
+    while(getline(cin,commandLine) && commandLine != "exit") {
+        Command *command = CommandParser::parse(commandLine, player);
+        if (command != nullptr) {
+            cout << command->execute() << endl;
+        } else {
+            cout << "Unknown command" << endl;
+        }
+    }
     return 0;
 }
